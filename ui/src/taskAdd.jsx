d0c209beb30900f0fe403d2fbe67c98c9cf9d5a2
Mampionona33 <ramamps33@gmail.com>
2021-09-09 14:45:17 +0300

    setting up the header for the table

--- ui/src/taskAdd.jsx
+++ ui/src/taskAdd.jsx
@@ -1,25 +1,6 @@
 class TaskAdd extends React.Component {
   render() {
-    const rowStyle = { border: '1.5px solid silver', padding: 4 };
-    return (
-      <table style={{ borderCollapse: 'collapse' }}>
-        <thead>
-          <tr>
-            <th style={rowStyle}>type de travail</th>
-            <th style={rowStyle}>booth number</th>
-            <th style={rowStyle}>status com</th>
-            <th style={rowStyle}>status ivpn</th>
-            <th style={rowStyle}>url fiche</th>
-            <th style={rowStyle}>état</th>
-            <th style={rowStyle}>nb avant</th>
-            <th style={rowStyle}>nb apres</th>
-            <th style={rowStyle}>date début</th>
-            <th style={rowStyle}>date validation</th>
-            <th style={rowStyle}>durée de production</th>
-          </tr>
-        </thead>
-      </table>
-    );
+    return <div>place holder for formulaire</div>;
   }
 }
 
--- ui/src/taskTable.jsx
+++ ui/src/taskTable.jsx
@@ -1,5 +1,26 @@
 export default class TaskTable extends React.Component {
   render() {
-    return <div>this is a placeholder for presenting a table .</div>;
+    const rowStyle = { border: '1.5px solid silver', padding: 4 };
+    return (
+      <table style={{ borderCollapse: 'collapse' }}>
+        <thead>
+          <tr>
+            <th style={rowStyle}>type de travail</th>
+            <th style={rowStyle}>booth number</th>
+            <th style={rowStyle}>status com</th>
+            <th style={rowStyle}>status ivpn</th>
+            <th style={rowStyle}>url fiche</th>
+            <th style={rowStyle}>état</th>
+            <th style={rowStyle}>nb avant</th>
+            <th style={rowStyle}>nb apres</th>
+            <th style={rowStyle}>date début</th>
+            <th style={rowStyle}>date validation</th>
+            <th style={rowStyle}>durée de production</th>
+            <th style={rowStyle}>productivité</th>
+            <th style={rowStyle}>Action</th>
+          </tr>
+        </thead>
+      </table>
+    );
   }
 }
