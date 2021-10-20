d0927750922abc9d4d6dfdc954357ad483541fc9
Mampionona33 <ramamps33@gmail.com>
2021-10-20 09:13:53 +0300

    adding schema.graphql and implementing root in server.js

---
+++ api/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// Construct a schema, using GraphQL schema language
+const db = require('./db.json');
+const fs = require('fs');
+const path = require('path');
+const { graphqlHTTP } = require('express-graphql');
+const { makeExecutableSchema } = require('graphql-tools');
+const schemaFile = path.join(__dirname, 'schema.graphql');
+const typeDefs = fs.readFileSync(schemaFile, 'utf-8');
+const schema = makeExecutableSchema({ typeDefs });
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  TableHeaderList: () => {
+    return db.tableHeader.map((txt) => {
+      return txt.value;
+    });
+  },
+  Fiche: () => {
+    return db.fiche.map((fiches) => {
+      return fiches;
+    });
+  },
+};
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`API server runing on ${PORT}`);
+});
