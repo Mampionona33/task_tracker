714d7c983c6544012e3b7b3bd47e835793585e1d
Mampionona33 <ramamps33@gmail.com>
2021-10-21 14:54:20 +0300

    connecting api to mongodb and modiffing db.json

--- api/server.js
+++ api/server.js
@@ -2,8 +2,31 @@ const express = require('express');
 const app = express();
 const PORT = 8000;
 
+const { MongoClient } = require('mongodb');
+const url =
+  'mongodb+srv://mampionona:daddy4806@cluster0.tksr3.mongodb.net/task_tracker?retryWrites=true';
+let db;
+
+// connect to mongodb database
+async function connectToDb() {
+  const client = new MongoClient(url, { useNewUrlParser: true });
+  await client.connect();
+  console.log('Connect to MongoDb at', url);
+  db = client.db();
+}
+
+async function getTableHeaderMongo() {
+  const tableHeader = await db.collection('tableHeader').distinct('value');
+  return tableHeader;
+}
+
+async function getFicheMongo() {
+  const fiches = await db.collection('fiches').find({}).toArray();
+  return fiches;
+}
+
 // Construct a schema, using GraphQL schema language
-const db = require('./db.json');
+// const db = require('./db.json'); //local database
 const fs = require('fs');
 const path = require('path');
 const { graphqlHTTP } = require('express-graphql');
@@ -15,14 +38,12 @@ const schema = makeExecutableSchema({ typeDefs });
 // The root provides a resolver function for each API endpoint
 const root = {
   GetTableHeaderList: () => {
-    return db.tableHeader.map((txt) => {
-      return txt.value;
-    });
+    const tableHeader = getTableHeaderMongo();
+    return tableHeader;
   },
   GetFiches: () => {
-    return db.fiches.map((fiches) => {
-      return fiches;
-    });
+    const fiches = getFicheMongo();
+    return fiches;
   },
 };
 
@@ -35,6 +56,13 @@ app.use(
   })
 );
 
-app.listen(PORT, () => {
-  console.log(`API server runing on ${PORT}`);
-});
+(async function () {
+  try {
+    await connectToDb();
+    app.listen(PORT, () => {
+      console.log(`API server runing on ${PORT}`);
+    });
+  } catch (err) {
+    console.log('ERROR:', err);
+  }
+})();
